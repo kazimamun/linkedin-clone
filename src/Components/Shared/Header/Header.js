2695fb7a8f9defb2aea103a2c04a5c494afb0738
Kazi Mamun <kazi299499@gmail.com>
2021-07-23 17:02:26 +0600

    second upload

--- src/Components/Shared/Header/Header.js
+++ src/Components/Shared/Header/Header.js
@@ -3,6 +3,9 @@ import './Header.css';
 import SearchIcon from '@material-ui/icons/Search';
 import HomeIcon from '@material-ui/icons/Home';
 import SupervisorAccountIcon from '@material-ui/icons/SupervisorAccount';
+import BusinessIcon from '@material-ui/icons/Business';
+import ChatIcon from '@material-ui/icons/Chat';
+import NotificationsIcon from '@material-ui/icons/Notifications';
 import HeaderOption from './HeaderOption';
 
 const Header = () => {
@@ -18,6 +21,9 @@ const Header = () => {
             <div className="header_right">
                 <HeaderOption Icon={HomeIcon} title="Home" />
                 <HeaderOption Icon={SupervisorAccountIcon} title="My Network" />
+                <HeaderOption Icon={BusinessIcon} title="Jobs" />
+                <HeaderOption Icon={ChatIcon} title="Messaging" />
+                <HeaderOption Icon={NotificationsIcon} title="Notification" />
             </div>
         </div>
     );
